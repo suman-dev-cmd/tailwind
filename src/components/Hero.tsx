d7af7f376ebda50e0092e740b3a79cd1796e4955
Suman Jana <sumanjana640@gmail.com>
2021-06-22 17:15:04 +0530

    tailwind css

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero:React.FC = () => {
             </h1>
             <Link
                 className='py-6 px-10 bg-yellow-500 rounded-full text-3xl hover:bg-yellow-300 transition duration-300 ease-in-out flex items-center animate-bounce'
-                to='/menu'
+                to='/gellary'
             >
                 Order Now{' '}
                 <svg
